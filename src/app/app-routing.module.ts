782abf89eabcebe157bc8936055ab68d707ec0dd
BivinBv <bivin.b@techversantinfo.com>
2023-03-21 18:02:36 +0530

    deisgned product page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './component/homepage/homepage.component';
 import { ProductsComponent } from './component/products/products.component';
 
 const routes: Routes = [
   {
     path:'products',component:ProductsComponent
+  },
+  {
+    path:'',component:HomepageComponent
   }
 
 ];
